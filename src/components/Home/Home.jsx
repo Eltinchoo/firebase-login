79998cde2b2e7af62b2224b5906ac7ea643d3c1d
Martin <mattpaganini07@gmail.com>
2025-02-27 18:00:24 -0300

    in progress

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import { auth } from "../../firebase";
 
 export default function Home(props) {
+  const navigate = useNavigate();
+  const logOut = () => {
+    navigate("/");
+    return auth.signOut();
+  };
+
   return (
     <div>
       <div>
-        <div>
-          <h1>
-            <Link to="/login">Login</Link>
-          </h1>
-          <br />
-          <h1>
-            <Link to="/singup">Registrarse</Link>
-          </h1>
-        </div>
+        <h1>
+          <Link to="/login">Accede</Link>
+        </h1>
+        <br />
+        <h1>
+          <Link to="/signup">Registrarse</Link>
+        </h1>
       </div>
       <h2>{props.name ? `Bienvenido ${props.name}` : "Iniciar sesión"}</h2>
+      <button onClick={logOut}>Salir</button>
     </div>
   );
 }
+
 Home.propTypes = {
   name: PropTypes.string.isRequired,
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,69 @@
 import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import InputControl from "../InputControl/InputControl";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    email: "",
+    pass: "",
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+  const iniciar = () => {
+    event.preventDefault();
+    if (!values.email || !values.pass) {
+      setErrorMsg("*Todos los campos son obligatorios");
+      return;
+    }
+    setErrorMsg("");
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.pass)
+      .then(async () => {
+        setSubmitButtonDisabled(false);
+        navigate("/signup");
+      })
+      .catch(() => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg("Error al iniciar sesión");
+      });
+  };
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
-        <h1 className={styles.heading}>Login</h1>
+        <h1 className={styles.heading}>Accede a tu cuenta</h1>
+
+        <InputControl
+          label="Email"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+          placeholder="Ingresa tu email"
+        />
+
+        <InputControl
+          label="Contraseña"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+          placeholder="Ingresa tu contraseña"
+        />
+
         <div className={styles.footer}>
-          <b className={styles.error}>Error</b>
-          <button>Login btn</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={iniciar} disabled={submitButtonDisabled}>
+            Login btn
+          </button>
           <p>
             Crear cuenta
             <span>
-              <Link to="/singup">Ir</Link>
+              <Link to="/signup">Ir</Link>
             </span>
           </p>
         </div>
